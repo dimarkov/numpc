4eb8e17046f479ae9ec57c6e94ca6ae5d851f7c6
dimarkov <5038100+dimarkov@users.noreply.github.com>
2022-11-13 14:03:01 +0100

    mean field approx with shrikage priors - not working

--- notebooks/bmr/models.py
+++ notebooks/bmr/models.py
@@ -1,11 +1,11 @@
-from signal import Sigmasks
 import jax.numpy as jnp
+import jax.tree_util as jtu
 import numpyro.distributions as dist
 import optax
 import equinox as eqx
 
 from functools import partial
-from jax import nn, lax, random, vmap, device_put
+from jax import nn, lax, random, vmap, device_put, linearize
 from numpyro import handlers, sample, plate, deterministic, factor, subsample, param
 from numpyro.infer import NUTS, MCMC, SVI, Trace_ELBO, TraceMeanField_ELBO, Predictive
 from numpyro.distributions import constraints
@@ -461,22 +461,38 @@ class BayesDNN(object):
         samples = None
         return params
 
+def substract(a, b):
+    return a - b
+
+def linearize_nnet(nnet, Ws, x):
+    params, static = eqx.partition(nnet, eqx.is_inexact_array)
+    def f(p):
+        nn = eqx.combine(p, static)
+        return vmap(nn)(x)
+
+    in_tangents = jtu.tree_map(substract, Ws, params)
+    
+    out, f_jvp = linearize(f, params)
+
+    return out + f_jvp(in_tangents)
 
 class BMRDNN(object):
-    def __init__(self, rng_key, nnet, images, tau_0=1, subsample_size=64):
+    def __init__(self, rng_key, nnet, images, tau_0=1, sigma_0=1, subsample_size=64, rank=5):
         self.N, self.D = images.shape
         self.images = images
 
         self.rng_key = rng_key
         self.tau_0 = tau_0
+        self.sigma_0 = sigma_0
 
         self.nnet = nnet
+
+        params, static = eqx.partition(self.nnet, eqx.is_inexact_array)
+        self.vals, self.aux = params.tree_flatten()
+
         self.subsample_size=subsample_size
 
-        self.params, self.static = eqx.partition(nnet, eqx.is_inexact_array)
-        self.vals, self.aux = self.params.tree_flatten()
-
-        self.rank = 2
+        self.rank = rank
 
     def likelihood(self, nnet, labels):
         with plate("N", self.N, subsample_size=self.subsample_size):
@@ -493,103 +509,112 @@ class BMRDNN(object):
             )
 
     def _register_network_vars(self):
+        params, static = eqx.partition(self.nnet, eqx.is_inexact_array)
+        vals, aux = params.tree_flatten()
         new_vals = []
-        for l, layer in enumerate(self.vals[0]):
+        for l, layer in enumerate(vals[0]):
+            lv, l_aux = layer.tree_flatten()
+            new_lv = ()
+            for value, name in zip(lv, l_aux[0]):
+                if value is not None:
+                    new_lv += (param(f'loc.layer{l}.{name}', value),)
+                else:
+                    new_lv += (value,)
+
+            new_vals.append( layer.tree_unflatten(l_aux, new_lv) )
+
+        vals = (new_vals,) + vals[1:]
+        params = params.tree_unflatten(aux, vals)
+
+        return eqx.combine(params, static)
+
+    def _register_random_network_vars(self, nnet):
+        params, static = eqx.partition(nnet, eqx.is_inexact_array)
+        vals, aux = params.tree_flatten()
+        new_vals = []
+        for l, layer in enumerate(vals[0]):
             lv, l_aux = layer.tree_flatten()
             new_lv = ()
             for value, name in zip(lv, l_aux[0]):
                 if value is not None:
                     if name == 'bias':
-                        new_lv += (sample(f'layer{l}.{name}', dist.Normal(0., 10.).expand(list(value.shape)).to_event(1)),)
+                        new_lv += (sample(f'layer{l}.{name}', dist.Normal(value, self.sigma_0).to_event(1)),)
                     else:
-                        new_lv += (sample(f'layer{l}.{name}', dist.Normal(0., 1.).expand(list(value.shape)).to_event(1)),)
+                        new_lv += (sample(f'layer{l}.{name}', dist.Normal(value, self.sigma_0).to_event(1)),)
                 else:
                     new_lv += (value,)
 
             new_vals.append( layer.tree_unflatten(l_aux, new_lv) )
 
         vals = (new_vals,) + self.vals[1:]
-        params = self.params.tree_unflatten(self.aux, vals)
+        params = params.tree_unflatten(aux, vals)
 
-        return eqx.combine(params, self.static)
+        return eqx.combine(params, static)
 
     def model(self, labels=None):
-        nnet = vmap(self._register_network_vars())
-        self.likelihood(nnet, labels)
+        d_nnet = self._register_network_vars()
+        r_nnet = self._register_random_network_vars(d_nnet)
+        Ws, _ = eqx.partition(r_nnet, eqx.is_inexact_array)
+        lin_nnet = partial(linearize_nnet, d_nnet, Ws)
+        self.likelihood(lin_nnet, labels)
 
-    def mv_guide(self, labels=None):
+    def guide(self, labels=None):
+        rank = self.rank
         for l, layer in enumerate(self.vals[0]):
             lv, l_aux = layer.tree_flatten()
             for value, name in zip(lv, l_aux[0]):
                 if value is not None:
                     if name == 'bias':
                         name = f'layer{l}.{name}'
-                        loc = param('loc.' + name, value)
-                        scale = param('scale.' + name, jnp.diag(jnp.ones(value.shape))/10, constraint=dist.constraints.lower_cholesky)
-                        sample(name, dist.MultivariateNormal(loc, scale_tril=scale))
+                        scale = param('scale.' + name, jnp.ones(value.shape)/10, constraint=dist.constraints.softplus_positive)
+                        sample(name, dist.Normal(0., scale).to_event(1))
                     else:
                         name = f'layer{l}.{name}'
-                        loc = param('mu.' + name, value)
                         i, j = value.shape
-                        cov = param('cov.' + name, jnp.zeros((i, j, j)))
-                        sample(name, dist.MultivariateNormal(loc, cov))
-
-    def lowrank_guide(self, labels=None):
-        for l, layer in enumerate(self.vals[0]):
-            lv, l_aux = layer.tree_flatten()
-            for value, name in zip(lv, l_aux[0]):
-                if value is not None:
-                    if name == 'bias':
-                        name = f'layer{l}.{name}'
-                        loc = param('loc.' + name, value)
-                        scale = param('scale.' + name, jnp.diag(jnp.ones(value.shape))/10, constraint=dist.constraints.lower_cholesky)
-                        sample(name, dist.MultivariateNormal(loc, scale_tril=scale))
-                    else:
-                        name = f'layer{l}.{name}'
-                        loc = param('loc.' + name, value)
-                        i, j = value.shape
-                        cov_diag = param('cov_diag.' + name, jnp.ones((i, j))/10, constraint=dist.constraints.positive)
-                        cov_factor = param('cov_factor.' + name, jnp.zeros((i, j, self.rank)))
-                        sample(name, dist.LowRankMultivariateNormal(loc, cov_factor, cov_diag))
+                        scale_u = param('scale_u.' + name, jnp.ones((i, rank))/jnp.sqrt(10 * rank))
+                        scale_v = param('scale_v.' + name, jnp.ones((rank, j))/jnp.sqrt(10 * rank))
+                        scale = deterministic('scale.' + name, jnp.abs(scale_u @ scale_v))
+                        sample(name, dist.Normal(0., scale).to_event(1))
         
+    def aux_guide(self, labels=None):
+        rank = self.rank
+        for l, layer in enumerate(self.vals[0]):
+            lv, l_aux = layer.tree_flatten()
+            for value, name in zip(lv, l_aux[0]):
+                if value is not None:
+                    if name == 'bias':
+                        name = f'layer{l}.{name}'
+                        scale = param('scale.' + name, jnp.ones(value.shape)/10, constraint=dist.constraints.softplus_positive)
+                        sample(name, dist.Normal(0., scale).to_event(1))
+                    else:
+                        name = f'layer{l}.{name}'
+                        i, j = value.shape
+                        scale = param('scale.' + name, jnp.ones((i, rank))/jnp.sqrt(10 * rank))
+                        sample(name, dist.Normal(0., scale).to_event(1))
+
     def gamma(self, c_sqr, v, u):
         v0 = jnp.expand_dims(v[..., 0], -1)
         u0 = jnp.expand_dims(u[..., 0], -1)
-        return self.tau_0**2 * v[..., 1:] * v0 / (c_sqr * u[..., 1:] * u0 + self.tau_0**2 * v[..., 1:] * v0)
+        return self.tau_0**2 * c_sqr * v[..., 1:] * v0/(c_sqr * u[..., 1:] * u0  + self.tau_0 * v[..., 1:] * v0)
 
-    def ΔF(self, mu, d, W, gamma):
-        # Σ = D + W @ W.T
-        Σ = jnp.diag(d) + W @ W.T
-        # C = I + W.T @ inv(D) @ W
-        C = jnp.eye(self.rank) + W.T @ (W/jnp.expand_dims(d, -1))
-        # D' = G + D @ (I - G)
-        _d = gamma + d * (1 - gamma)  
-        # W' = (I - G) @ W
-        _W = jnp.expand_dims(1 - gamma, -1) * W
-        # M = D' + W @ W'.T
-        M = jnp.diag(_d) + W @ _W.T
-        # C' = I + W'.T @ inv(D') @ W
-        _C = jnp.eye(self.rank) + _W.T @ ( W/jnp.expand_dims(_d, -1) )         
+    def ΔF(self, μ, σ_sqr, γ_sqr, π_0=1.):
+        # π_{lij} = 1/σ^2
+        π = 1/σ_sqr
+
+        # m_{lij} = γ² + σ² - π₀σ²γ²
+
+        m = γ_sqr + σ_sqr - π_0 * σ_sqr * γ_sqr
+
+        # \tilde{σ}^2_{lij} = γ² σ² / m
+        _σ_sqr = γ_sqr * σ_sqr / m
+
+        # \tilde{μ}_{lij} = π_{lij} μ_{lij} / \tilde{π}_{lij}
+        _μ = π * μ * _σ_sqr
         
-        # log|M| = log|D'| + log|C'|
-        df = - jnp.log(_d).sum()/2
+        df = - jnp.log(m).sum()
+        df = df - jnp.sum(μ**2 * π * (1 - γ_sqr/m))
 
-        _, logdet = jnp.linalg.slogdet(_C)
-        df -= logdet/2
-
-        # inv(M) = inv(D') - inv(D') @ W @ inv(C') @ W'.T @ inv(D') = inv(D') - R
-        # Σ' = G @ inv(M) @ Σ = G @ inv(D') @ D - G @ R @ D + G @ inv(D') @ W @ W.T - G @ R @ W @ W.T
-        _Σ = jnp.expand_dims(gamma, -1) * jnp.linalg.solve(M, Σ)
-
-        # mu' = G @ inv(M) @ mu
-        _μ = gamma * jnp.linalg.solve(M, mu)
-
-        # inv(Σ) = inv(D) - inv(D) @ W @ inv(C) @ W.T @ inv(D)
-        tmp = _μ/d
-        tmp = tmp - (W @ jnp.linalg.solve(C, W.T @ tmp))/d
-        df += jnp.dot(mu, tmp)/2
-
-        return df, _μ, _Σ
+        return df/2, _μ, jnp.sqrt(_σ_sqr)
             
     def bmr_model(self, params):
         for l, layer in enumerate(self.vals[0]):
@@ -606,30 +631,22 @@ class BMRDNN(object):
 
                             tau = deterministic(f'{name}.tau', self.tau_0 * jnp.sqrt(v[..., 0]/u[..., 0]))
                             lam = deterministic(f'{name}.lam', jnp.expand_dims(tau, -1) * jnp.sqrt(v[..., 1:]/u[..., 1:]))
-                            g = deterministic(f'{name}.gamma^2', self.gamma(c_sqr, v, u))
+                            γ_sqr = deterministic(f'{name}.gamma^2', self.gamma(c_sqr, v, u))
                             
-                            mu = params['loc.' + name]
-                            cov_diag = params['cov_diag.' + name]
-                            cov_factor = params['cov_factor.' + name]
+                            μ = params['loc.' + name]
+                            u = params['scale_u.' + name]
+                            v = params['scale_v.' + name]
+                            σ_sqr = jnp.square(u @ v)
 
-                            log_prob, t_mu, t_Sigma = vmap(self.ΔF)(mu, cov_diag, cov_factor, g)
-                            deterministic(f'mu.{name}', t_mu)
-                            deterministic(f'cov.{name}', t_Sigma)
+                            log_prob, t_mu, t_scale = vmap(self.ΔF)(μ, σ_sqr, γ_sqr, π_0 = 1/self.sigma_0**2)
+                            deterministic(f'loc.{name}', t_mu)
+                            deterministic(f'scale.{name}', t_scale)
                             factor(f'{name}.log_prob', log_prob)
 
     def _log_normal(self, name, shape, scale=.1):
         loc = param(f'{name}.loc', jnp.zeros(tuple(shape)))
         scale = param(f'{name}.scale', scale*jnp.ones(tuple(shape)), constraint=dist.constraints.softplus_positive)
         return sample(name, dist.LogNormal(loc, scale).to_event(1))
-
-    def _log_lowrank_normal(self, name, shape, scale=.1):
-        loc = param(f'{name}.loc', jnp.zeros(tuple(shape)))
-        cov_diag = param(f'{name}.cov_diag', scale*jnp.ones(tuple(shape)), constraint=dist.constraints.softplus_positive)
-        cov_factor = param('cov_factor.' + name, jnp.zeros(tuple(shape) + (self.rank,)))
-
-        Exp = ExpTransform()
-        mvn = dist.LowRankMultivariateNormal(loc, cov_factor, cov_diag)
-        sample(name, dist.TransformedDistribution(mvn, Exp))
 
     def bmr_guide(self, params):
         for l, layer in enumerate(self.vals[0]):
@@ -643,8 +660,8 @@ class BMRDNN(object):
                         scale_c = param(f'{name}.c^2.scale', jnp.ones(1), constraint=dist.constraints.softplus_positive)
                         sample(f'{name}.c^2', dist.LogNormal(loc_c, scale_c))
                         with plate(name, shape[0]):
-                            self._log_lowrank_normal(f'{name}.u', [shape[0], shape[-1] + 1])
-                            self._log_lowrank_normal(f'{name}.v', [shape[0], shape[-1] + 1])
+                            self._log_normal(f'{name}.u', [shape[0], shape[-1] + 1])
+                            self._log_normal(f'{name}.v', [shape[0], shape[-1] + 1])
 
     def run_svi(self, rng_key, num_steps, model, guide, optimizer, loss, *args, **kwargs):
         svi = SVI(model, guide, optimizer, loss)
@@ -652,7 +669,7 @@ class BMRDNN(object):
 
     def fit(self, labels, num_steps=1000, num_particles=1, opt_kwargs={'learning_rate': 1e-3}):
         _model = self.model
-        _guide = self.lowrank_guide
+        _guide = self.guide
         optimizer = optax_to_numpyro(optax.chain(optax.adabelief(**opt_kwargs)))
 
         self.rng_key, _rng_key = random.split(self.rng_key)
@@ -674,8 +691,8 @@ class BMRDNN(object):
         bmr_res = self.run_svi(_rng_key, num_steps, _model, _guide, optimizer, Trace_ELBO(num_particles), params)
 
         L = self.nnet.depth + 1
-        return_sites = [f'mu.layer{l}.weight' for l in range(L)] 
-        return_sites += [f'cov.layer{l}.weight' for l in range(L)]
+        return_sites = [f'loc.layer{l}.weight' for l in range(L)] 
+        return_sites += [f'scale.layer{l}.weight' for l in range(L)]
         pred = Predictive(_model, guide=_guide, params=bmr_res.params, num_samples=num_samples, return_sites=return_sites)
         self.rng_key, _rng_key = random.split(self.rng_key)
         samples = pred(_rng_key, params)
