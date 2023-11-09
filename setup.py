cb96d0de4b70e4e0354a6aad638accb7045f14e1
dimarkov <5038100+dimarkov@users.noreply.github.com>
2023-11-09 10:42:23 +0100

    updated sparse bayes

--- setup.py
+++ setup.py
@@ -1,31 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-packages = \
-['numpc']
-
-package_data = \
-{'': ['*']}
-
-install_requires = \
-[]
-
-setup_kwargs = {
-    'name': 'numpc',
-    'version': '0.1.0',
-    'description': '',
-    'long_description': None,
-    'author': 'dimarkov',
-    'author_email': '5038100+dimarkov@users.noreply.github.com',
-    'maintainer': None,
-    'maintainer_email': None,
-    'url': None,
-    'packages': packages,
-    'package_data': package_data,
-    'install_requires': install_requires,
-    'python_requires': '>=3.10',
-}
-
-
-setup(**setup_kwargs)
+if __name__ == "__main__":
+    setup()
 
